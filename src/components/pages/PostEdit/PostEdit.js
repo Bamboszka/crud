ff7c45d5383edb7339b6678a83bc95350e767886
Paulina Ruta <paulina.ruta@outlook.com>
2023-07-08 12:22:27 +0200

    Add function edit

--- src/components/pages/PostEdit/PostEdit.js
+++ src/components/pages/PostEdit/PostEdit.js
@@ -1,7 +1,10 @@
+import EditPostForm from '../../features/EditPostForm/EditPostForm.js';
+
 const PostEdit = () => {
     return (
         <div>
             <h1>Post edit</h1>
+            <EditPostForm />
         </div>
     );
 };
